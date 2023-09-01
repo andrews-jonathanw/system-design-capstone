d674bc3ed3cd8d0c4fdeaedc8a84f574d3f2e4c9
Tonegawa94 <victor1394@gmail.com>
2023-09-01 13:22:55 -0500

    small changes

---
+++ server/db/review-server/reviewsController.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const db = require('../db');
+
+const reviewsRouter = express.Router();
+
+reviewsRouter.get('/', (req, res) => {
+  // add functionality to account for headers on count and page
+  db.query(`SELECT * FROM product WHERE ...`);
+  // respond w data - transformed to how front end needs it
+});
+
+reviewsRouter.get('/:product_id', (req, res) => {
+  // return product w specific id
+  db.query(`SELECT * FROM product WHERE product_id = ${req.params.product_id}`);
+  // respond w data - transformed to how front end needs it
+});
+
+reviewsRouter.get('/:product_id/styles', (req, res) => {
+  // return all styles from id
+  db.query(`SELECT * FROM styles WHERE product_id = ${req.params.product_id}`);
+  // also need photos and skus
+  // respond w data - transformed to how front end needs it
+});
+
+module.exports = reviewsRouter;
