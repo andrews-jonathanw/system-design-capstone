faa04225145f32998da5f6c958ca22ca37ba2622
benjamin <bnt922@gmail.com>
2023-09-01 16:30:42 -0400

    Working routes for products and product_id

--- server/product-server/controllers.js
+++ server/product-server/controllers.js
@@ -4,16 +4,28 @@ const db = require('../db');
 const productRouter = express.Router();
 
 productRouter.get('/', async (req, res) => {
-  // add functionality to account for headers on count and page
-  db.query(`SELECT * FROM product WHERE ...`);
-  // respond w data - transformed to how front end needs it
+  const count = req.query.count || 5;
+  const page = req.query.page || 1;
+  console.log()
+  const query = await db.query(`SELECT * FROM product WHERE product.id >= ${(count * page) - (count - 1)} AND product.id <= ${page * count}`);
+  res.send(query.rows);
 });
 
 productRouter.get('/:product_id', async (req, res) => {
-  // return product w specific id
-  const query = await db.query(`SELECT * FROM product WHERE id = ${req.params.product_id}`);
+  const query = await db.query(`
+  SELECT
+    product.id, product.name, product.slogan, product.description, product.category, product.default_price,
+    ARRAY_AGG(row_to_json(f)) features
+  FROM
+    product
+  INNER JOIN
+    (SELECT product_id, feature, value FROM features) f ON f.product_id = product.id
+  WHERE
+    product.id = ${req.params.product_id}
+  GROUP BY
+   product.id
+  `)
   res.send(query.rows[0]);
-  // respond w data - transformed to how front end needs it
 });
 
 productRouter.get('/:product_id/styles', (req, res) => {
