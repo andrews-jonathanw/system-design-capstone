3f2e62e56501af41a34420c23f8c5f3d68fc4671
benjamin <bnt922@gmail.com>
2023-09-02 14:34:16 -0400

    queries complete thx for the broken csv files hack reactor

--- server/product-server/controllers.js
+++ server/product-server/controllers.js
@@ -6,7 +6,14 @@ const productRouter = express.Router();
 productRouter.get('/', async (req, res) => {
   const count = req.query.count || 5;
   const page = req.query.page || 1;
-  const query = await db.query(`SELECT * FROM product WHERE product.id >= ${(count * page) - count + 1} AND product.id <= ${page * count}`);
+  const query = await db.query(`
+    SELECT
+      *
+    FROM
+      product
+    WHERE
+      product.id >= ${(count * page) - count + 1} AND product.id <= ${page * count}
+  `);
   res.send(query.rows);
 });
 
@@ -17,7 +24,7 @@ productRouter.get('/:product_id', async (req, res) => {
     ARRAY_AGG(JSON_BUILD_OBJECT('Feature', features.feature, 'Value', features.value)) AS features
   FROM
     product
-  LEFT JOIN
+  INNER JOIN
     features ON features.product_id = product.id
   WHERE
     product.id = ${req.params.product_id}
@@ -28,14 +35,13 @@ productRouter.get('/:product_id', async (req, res) => {
 });
 
 productRouter.get('/:product_id/styles', async (req, res) => {
-  // will return all styles from id some day
   const query = await db.query(`
   SELECT
     styles.id AS style_id,
     styles.name AS name,
-    styles.original_price AS original_price
+    styles.original_price AS original_price,
     styles.sale_price AS sale_price,
-    styles.default_style AS 'default?',
+    styles.default_style AS "default?",
     ARRAY_AGG(DISTINCT
       JSONB_BUILD_OBJECT(
         'thumbnail_url', photos.thumbnail_url,
@@ -49,7 +55,7 @@ productRouter.get('/:product_id/styles', async (req, res) => {
       )
     ) AS skus
   FROM
-    styles
+  styles
   INNER JOIN
     photos ON photos.style_id = styles.id
   INNER JOIN
@@ -59,45 +65,15 @@ productRouter.get('/:product_id/styles', async (req, res) => {
   GROUP BY
     styles.id
   `);
-  res.send(query.rows);
+  let response = {
+    product_id: `${req.params.product_id}`,
+    results: query.rows
+  }
+  response.results.forEach(style => {
+    style['default?'] = (style['default?'] === '1' ? true : false);
+    style['sale_price'] = (style['sale_price'] === 'null' ? null : Number(style['sale_price']));
+  })
+  res.send(response);
 });
 
 module.exports = productRouter;
-
-/*
-  SELECT
-  product.id AS product_id,
-    (SELECT
-      styles.id AS style_id,
-      styles.name AS name,
-      styles.original_price AS original_price
-      styles.sale_price AS sale_price,
-      styles.default_style AS 'default?',
-      ARRAY_AGG(DISTINCT
-        JSONB_BUILD_OBJECT(
-          'thumbnail_url', photos.thumbnail_url,
-          'url', photos.url
-        )
-      ) AS photos,
-      JSON_OBJECT_AGG(
-        skus.id, JSON_BUILD_OBJECT(
-          'quantity', skus.quantity,
-          'size', skus.size
-        )
-      ) AS skus
-      FROM
-        styles
-      INNER JOIN
-        photos ON photos.style_id = styles.id
-      INNER JOIN
-        skus ON skus.style_id = styles.id
-      WHERE
-        styles.product_id = ${req.params.product_id}
-      GROUP BY
-        styles.id
-    ) AS results
-  FROM
-    product
-  WHERE
-    product.id = ${req.params.product_id}
-*/
